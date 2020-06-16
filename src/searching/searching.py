5e7689573209ca46148ccf3dedfa0e7b97c2203c
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-06-16 15:39:00 -0600

    searching and sorting done and tested

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,24 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    # base case - arr has len 0 or 1 return that arr
+    if len(arr) < 1:
+        return -1
+    if start == end:
+        return start
+
+    middle_index = (start+end)//2
+    # get 'middle' index and check if that's the value
+    if arr[middle_index] == target:
+        return middle_index
+        # if 'middle' is less than the value, update the start to be middle + 1
+    elif arr[middle_index] < target:
+        start = (start+end)//2 + 1
+    # else, update the end to be middle - 1
+    else:
+        end = (start+end)//2 - 1
+    
+    return binary_search(arr, target, start, end)
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +27,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
+# def agnostic_binary_search(arr, target):
+#     # Your code here
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,8 +2,23 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
+    index = 0
 
-    # Your code here
+    # Compare first element in each list.
+
+    while arrA and arrB:  # While elements are in both aarA and aarB
+        if arrA[0] < arrB[0]:  # if the first element in aarA is less
+            merged_arr[index] = (arrA.pop(0))  # pop it from aarA and put in merged_aar
+        else:
+            merged_arr[index] = (arrB.pop(0))  # pop first element of aarB to put in merged_aar
+        index += 1
+
+    if not arrA:  # If aarA is empty
+      for j in range(index, len(merged_arr)):  # Loop through aarB and add to merged_aar
+        merged_arr[j] = (arrB.pop(0))
+    else:  # If aarB is empty
+      for j in range(index, len(merged_arr)):  # Loop through aarA and add to merged_aar
+        merged_arr[j] = (arrA.pop(0))
 
 
     return merged_arr
@@ -11,18 +26,25 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
+    # Split arr in half and repeat until each element is in it's own list
+    left = arr[:len(arr)//2]
+    right = arr[len(arr)//2:]
 
+    left = merge_sort(left)
+    right = merge_sort(right)
 
-    return arr
+    return merge(left, right)
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
